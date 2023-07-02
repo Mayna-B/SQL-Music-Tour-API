680a5382726bb35aff0db97e07f9cc7b42979305
Mayna-B <me2be2014@gmail.com>
2023-07-02 13:18:09 -0400

    adding associations

--- models/meetgreet.js
+++ models/meetgreet.js
@@ -1,31 +1,41 @@
-'use strict';
+'use strict'
 const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Meet_greet extends Model {
+  class MeetGreet extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Band, Event }) {
+      // band
+      MeetGreet.belongsTo(Band, {
+        foreignKey: "band_id",
+        as: "band"
+      })
+
+      // event
+      MeetGreet.belongsTo(Event, {
+        foreignKey: "event_id",
+        as: "event"
+      })
     }
   }
-  Meet_greet.init({
+  MeetGreet.init({
     meet_greet_id: {
       type: DataTypes.INTEGER,
-      primaryKey:true,
+      primaryKey: true,
       autoIncrement: true
     },
     event_id: {
-      type: DataTypes.INTEGER,
-      allowNull:false
+      type: DataTypes.SMALLINT,
+      allowNull: false
     },
-    band_id:{
-      type: DataTypes.INTEGER,
-      allowNull:false
+    band_id: {
+      type: DataTypes.SMALLINT,
+      allowNull: false
     },
     meet_start_time: {
       type: DataTypes.DATE,
@@ -37,9 +47,9 @@ module.exports = (sequelize, DataTypes) => {
     }
   }, {
     sequelize,
-    modelName: 'Meet_greet',
+    modelName: 'MeetGreet',
     tableName: 'meet_greets',
     timestamps: false
-  });
-  return Meet_greet;
-};
+  })
+  return MeetGreet
+}
--- models/stage.js
+++ models/stage.js
@@ -1,26 +1,37 @@
-'use strict';
+'use strict'
 const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class stage extends Model {
+  class Stage extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Event, StageEvent, SetTime }) {
+      // events 
+      Stage.belongsToMany(Event, {
+        foreignKey: "stage_id",
+        as: "events",
+        through: StageEvent
+      })
+
+      // set times 
+      Stage.hasMany(SetTime, {
+        foreignKey: "stage_id",
+        as: "set_times"
+      })
     }
   }
-  stage.init({
+  Stage.init({
     stage_id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true
     },
-    stage_name:{
-      type:  DataTypes.STRING,
+    stage_name: {
+      type: DataTypes.STRING,
       allowNull: false
     }
   }, {
@@ -28,6 +39,6 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'Stage',
     tableName: 'stages',
     timestamps: false
-  });
-  return stage;
-};
+  })
+  return Stage
+}
